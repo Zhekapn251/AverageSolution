5f3754e1c9f7c643abc998f401df8a2242b84c36
DESKTOP-UQI3K99\Zheka <brahistohrona@gmail.com>
2022-12-08 19:53:45 +0200

    intial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32Adder int32Adder = new Int32Adder();
+            List<int> numbers= new List<int>{1, 2, 3, 4,};
+            int summ = numbers.ExtensionSum(int32Adder);
+            Console.WriteLine(summ);
+            
+            FloatAdder floatAdder = new FloatAdder();
+            List<float> floatNumbers= new List<float>{1.2f, 2.5f, 3.5f, 4.5f,};
+            float floatSumm = floatNumbers.ExtensionSum(floatAdder);
+            Console.WriteLine(floatSumm);
+
+            StringAdder stringAdder = new StringAdder();
+            List<string> words = new List<string> {"1", "2", "3", "4"};
+            string text = words.ExtensionSum(stringAdder);
+            Console.WriteLine(text);
+
+
+            Console.WriteLine(Average(2,3));
+            
+            float Average(uint firstNumber, uint secondNumber)
+            {
+                return (firstNumber + secondNumber) / 2f;
+            }
+
+
+        }
+
+      
+        
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+
+    public static class TestExtension
+    {
+        public static  T ExtensionSum<T> (this IEnumerable<T> items, IAdder<T> adder) 
+        {
+            T count = adder.Zero;
+            
+            foreach (var item in items)
+            {
+                count = adder.Add(count,item);
+            }
+            return count;
+        }   
+    }
+    
+    public interface IAdder<T>
+    {
+        T Zero { get; }
+
+        T Add(T a, T b);
+    }
+    
+    public class Int32Adder : IAdder<Int32>
+    {
+        public int Zero { get { return 0; } }
+
+        public int Add(int a, int b) { return a + b; }
+    }
+    public class FloatAdder : IAdder<float>
+    {
+        
+        public float Zero { get { return 0; } }
+
+        public float Add(float a, float b) { return a + b; }
+    }
+    public class StringAdder : IAdder<string>
+    {
+        
+        public string Zero { get { return default; } }
+
+        public string Add(string a, string b) { return a + b; }
+    }
+}
